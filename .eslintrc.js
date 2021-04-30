6e882bff51ca130bc9a02fd69b3f315716a01970
Wasim Almadfaa <49785547+wmadfaa@users.noreply.github.com>
2021-04-30 11:24:51 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["dist"],
+  rules: {
+    "no-console": 2,
+    "comma-dangle": ["error", "always-multiline"],
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    "no-multi-spaces": "error"
+  }
+};
